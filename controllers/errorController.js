31de062db4026fc6b5d079121ff325683f852ab8
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-10-17 17:55:08 +0300

    handle development vs production errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,38 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    err,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //If operational, trusted error. Send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    //Programming/unknown error. Don't leak error details.
+    console.error('ERROR 💥');
+
+    //Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Someething went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
