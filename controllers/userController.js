d46446d71ff907f6918d520949a4c04b5e5ebdd3
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-11-02 16:03:51 +0300

    delete current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,6 +50,15 @@ exports.updateMe = async (req, res, next) => {
   });
 };
 
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    staus: 'success',
+    data: null,
+  });
+});
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
