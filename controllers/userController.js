d6693e63187b3a132ed53c56d1efa6d34a463020
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-11-02 15:00:29 +0300

    update current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -12,6 +21,34 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.updateMe = async (req, res, next) => {
+  //check whether user is trying to update password
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'Cannot update password on this route. Please visit /updatePassword',
+        400
+      )
+    );
+  }
+
+  // filter unwanted fields such as user role
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  //find and update user based on the filtered object
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+};
 
 exports.createUser = (req, res) => {
   res.status(500).json({
