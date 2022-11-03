ffe65b6b194c79c8df895688c6e26fd5a2739716
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-11-03 17:16:07 +0300

    protect against parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -53,6 +54,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against cross site scripting
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'price',
+      'difficulty',
+      'maxGroupSize',
+      'ratingsAverage',
+      'ratingsQuantity',
+    ],
+  })
+);
 
 // Test middleware
 app.use((req, res, next) => {
