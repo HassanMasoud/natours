fbc9ec943a7005af13d94666a4e5227f8455b3bc
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-11-14 19:19:19 +0300

    calculate average tour ratings

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -42,6 +43,55 @@ reviewSchema.pre(/^find/, function (next) {
   });
 
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    // find all reviews with the tourId
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        // add 1 for each document returned
+        nRatings: { $sum: 1 },
+        // calculate and store the average of the rating field only
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRatings,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4,
+    });
+  }
+};
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // find the current review and save it. This allows us to have access to it in the next middleware.
+  // we can't do the calculations since the document has not been updated
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // After updating the document, calculate the averages using the tour id found in r that was
+  // passed in by the previous middleware
+  await this.r.constructor.calcAverageRatings(this.r.tour);
+});
+
+reviewSchema.post('save', function () {
+  // this points to the current review. This is why we have access to the tourId
+  // this.constructor points to the current model
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
