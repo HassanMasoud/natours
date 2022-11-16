953c2913b25f90759f4bccea27fc9e8f4a453adc
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-11-16 07:49:27 +0300

    extend base template with blocks

--- app.js
+++ app.js
@@ -82,8 +82,19 @@ app.use((req, res, next) => {
 //MOUNT ROUTERS
 app.get('/', (req, res) => {
   res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Hassan',
+    title: 'Exciting Tours For Adventurous People',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
   });
 });
 
