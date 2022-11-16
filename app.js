4f8b3be9edcb2e3820ac786b3104a1fbd700d368
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-11-16 06:50:27 +0300

    pug fundamentals

--- app.js
+++ app.js
@@ -79,11 +79,14 @@ app.use((req, res, next) => {
   next();
 });
 
+//MOUNT ROUTERS
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Hassan',
+  });
 });
 
-//MOUNT ROUTERS
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
