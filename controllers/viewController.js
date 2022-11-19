6e4db9247083f9cabdd25195a174257769550801
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-11-19 12:59:58 +0300

    refactor code

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -11,14 +11,14 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.find({ slug: req.params.slug }).populate({
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
   });
 
   console.log(tour);
   res.status(200).render('tour', {
-    title: tour[0].name,
-    tour: tour[0],
+    title: tour.name,
+    tour,
   });
 });
