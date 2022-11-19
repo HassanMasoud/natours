2e3c863f16c09f39eec4779dc8e09c57414d7f1f
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-11-19 12:31:36 +0300

    build tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
 
   res.status(200).render('overview', {
@@ -10,8 +10,15 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.find({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  console.log(tour);
+  res.status(200).render('tour', {
+    title: tour[0].name,
+    tour: tour[0],
+  });
+});
