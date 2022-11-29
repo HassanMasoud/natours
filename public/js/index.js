63298c28fabf327545f2eb061076d2965a144eb8
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-11-29 18:36:51 +0300

    log out users

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
 import '@babel/polyfill';
-import { login } from './login';
+import { login, logout } from './login';
 import { displayMap } from './mapbox';
 
 // DOM Elements
 const mapbox = document.getElementById('map');
 const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 if (mapbox) {
   const locations = JSON.parse(mapbox.dataset.locations);
@@ -18,4 +19,8 @@ if (loginForm) {
     const password = document.getElementById('password').value;
     login(email, password);
   });
+}
+
+if (logOutBtn) {
+  logOutBtn.addEventListener('click', logout);
 }
