7b0635169fe45ac1a444a993c176cd6006adeb09
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-11-29 07:02:17 +0300

    log in users

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -66,6 +67,7 @@ app.use(
     limit: '10kb',
   })
 );
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -90,6 +92,7 @@ app.use(
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString(0);
+  console.log(req.cookies);
   next();
 });
 
